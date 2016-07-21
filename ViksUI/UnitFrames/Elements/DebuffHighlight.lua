ee5d3a25a82fb6941726282acec0c08d898e31bd
Tryllemann <frode.h@aktivportal.com>
2016-07-21 21:21:00 +0200

    Cleanup

--- ViksUI/UnitFrames/Elements/DebuffHighlight.lua
+++ ViksUI/UnitFrames/Elements/DebuffHighlight.lua
@@ -1,16 +1,22 @@
-if not oUF then return end
+local T, C, L = unpack(select(2, ...))
+if C.unitframe.enableDebuffHighlight ~= true then return end
 
-local playerClass = select(2,UnitClass("player"))
+----------------------------------------------------------------------------------------
+--	Based on oUF_DebuffHighlight(by Ammo)
+----------------------------------------------------------------------------------------
+local _, ns = ...
+local oUF = ns.oUF
+
 local CanDispel = {
-	PRIEST = { Magic = true, Disease = true, },
-	SHAMAN = { Magic = false, Curse = true, },
-	PALADIN = { Magic = false, Poison = true, Disease = true, },
-	MAGE = { Curse = true, },
-	DRUID = { Magic = false, Curse = true, Poison = true, },
-	MONK = { Magic = false, Curse = true, Poison = true, }
+	DRUID = {Magic = false, Curse = true, Poison = true},
+	MAGE = {Curse = true},
+	MONK = {Magic = false, Poison = true, Disease = true},
+	PALADIN = {Magic = false, Poison = true, Disease = true},
+	PRIEST = {Magic = false, Disease = false},
+	SHAMAN = {Magic = false, Curse = true}
 }
 
-local dispellist = CanDispel[playerClass] or {}
+local dispellist = CanDispel[T.class] or {}
 local origColors = {}
 local origBorderColors = {}
 local origPostUpdateAura = {}
@@ -28,28 +34,49 @@ local function GetDebuffType(unit, filter)
 	end
 end
 
-
 local function CheckSpec()
-	local role = ""
-	local tree = GetSpecialization()
-	
-	if tree then
-		role = select(6, GetSpecializationInfo(tree))
-	end
-	
-	if role == "HEALER" then
-		dispellist.Magic = true
-	else
-		dispellist.Magic = false
+	local spec = GetSpecialization()
+
+	if T.class == "DRUID" then
+		if spec == 4 then
+			dispellist.Magic = true
+		else
+			dispellist.Magic = false
+		end
+	elseif T.class == "MONK" then
+		if spec == 2 then
+			dispellist.Magic = true
+		else
+			dispellist.Magic = false
+		end
+	elseif T.class == "PALADIN" then
+		if spec == 1 then
+			dispellist.Magic = true
+		else
+			dispellist.Magic = false
+		end
+	elseif T.class == "PRIEST" then
+		if spec == 3 then
+			dispellist.Magic = false
+			dispellist.Disease = false
+		else
+			dispellist.Magic = true
+			dispellist.Disease = true
+		end
+	elseif T.class == "SHAMAN" then
+		if spec == 3 then
+			dispellist.Magic = true
+		else
+			dispellist.Magic = false
+		end
 	end
 end
 
-
 local function Update(object, event, unit)
-	if object.unit ~= unit  then return end
-	local debuffType, texture  = GetDebuffType(unit, object.DebuffHighlightFilter)
+	if object.unit ~= unit then return end
+	local debuffType, texture = GetDebuffType(unit, object.DebuffHighlightFilter)
 	if debuffType then
-		local color = DebuffTypeColor[debuffType] 
+		local color = DebuffTypeColor[debuffType]
 		if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
 			if object.DebuffHighlightBackdrop then
 				object:SetBackdropColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 1)
@@ -60,7 +87,7 @@ local function Update(object, event, unit)
 		elseif object.DebuffHighlightUseTexture then
 			object.DebuffHighlight:SetTexture(texture)
 		else
-			object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or .5)
+			object.DebuffHighlight:SetVertexColor(color.r, color.g, color.b, object.DebuffHighlightAlpha or 0.5)
 		end
 	else
 		if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
@@ -83,30 +110,28 @@ local function Update(object, event, unit)
 end
 
 local function Enable(object)
-	-- if we're not highlighting this unit return
+	-- If we're not highlighting this unit return
 	if not object.DebuffHighlightBackdrop and not object.DebuffHighlightBackdropBorder and not object.DebuffHighlight then
 		return
 	end
-	-- if we're filtering highlights and we're not of the dispelling type, return
-	if object.DebuffHighlightFilter and not CanDispel[playerClass] then
+	-- If we're filtering highlights and we're not of the dispelling type, return
+	if object.DebuffHighlightFilter and not CanDispel[T.class] then
 		return
 	end
-	
-	-- make sure aura scanning is active for this object
+
+	-- Make sure aura scanning is active for this object
 	object:RegisterEvent("UNIT_AURA", Update)
 	object:RegisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
-	object:RegisterEvent("CHARACTER_POINTS_CHANGED", CheckSpec)
 	CheckSpec()
-	
+
 	if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder then
 		local r, g, b, a = object:GetBackdropColor()
-		origColors[object] = { r = r, g = g, b = b, a = a}
+		origColors[object] = {r = r, g = g, b = b, a = a}
 		r, g, b, a = object:GetBackdropBorderColor()
-		origBorderColors[object] = { r = r, g = g, b = b, a = a}
-	elseif not object.DebuffHighlightUseTexture then -- color debuffs
-		-- object.DebuffHighlight
+		origBorderColors[object] = {r = r, g = g, b = b, a = a}
+	elseif not object.DebuffHighlightUseTexture then
 		local r, g, b, a = object.DebuffHighlight:GetVertexColor()
-		origColors[object] = { r = r, g = g, b = b, a = a}
+		origColors[object] = {r = r, g = g, b = b, a = a}
 	end
 
 	return true
@@ -116,10 +141,9 @@ local function Disable(object)
 	if object.DebuffHighlightBackdrop or object.DebuffHighlightBackdropBorder or object.DebuffHighlight then
 		object:UnregisterEvent("UNIT_AURA", Update)
 		object:UnregisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
-		object:UnregisterEvent("CHARACTER_POINTS_CHANGED", CheckSpec)
 	end
 end
 
-oUF:AddElement('DebuffHighlight', Update, Enable, Disable)
+oUF:AddElement("DebuffHighlight", Update, Enable, Disable)
 
 for i, frame in ipairs(oUF.objects) do Enable(frame) end
--- ViksUI/UnitFrames/Elements/raidDebuffs.lua
+++ ViksUI/UnitFrames/Elements/raidDebuffs.lua
@@ -1,147 +1,292 @@
+local T, C, L = unpack(select(2, ...))
+if C.unitframe.enable ~= true or C.unitframe.showRaidDebuffs ~= true then return end
+
+----------------------------------------------------------------------------------------
+--	Based on oUF_RaidDebuffs(by Yleaf)
+----------------------------------------------------------------------------------------
 local _, ns = ...
+local oUF = ns.oUF
 local cfg = ns.cfg
 local lib = ns.lib
-local oUF = ns.oUF or oUF
 
-local shadows = {
-	edgeFile = cfg.shadowedge, 
-	edgeSize = 4,
-	insets = { left = 3, right = 3, top = 3, bottom = 3 }
+local bossDebuffPrio = 9999999
+local invalidPrio = -1
+local auraFilters = {
+	["HARMFUL"] = true,
+}
+
+local DispellColor = {
+	["Magic"]	= {0.2, 0.6, 1},
+	["Curse"]	= {0.6, 0, 1},
+	["Disease"]	= {0.6, 0.4, 0},
+	["Poison"]	= {0, 0.6, 0},
+	["none"]	= {unpack(C.media.border_color)},
+}
+
+if C.aura.debuff_color_type == true then
+	DispellColor.none = {1, 0, 0}
+end
+
+local DispellPriority = {
+	["Magic"]	= 4,
+	["Curse"]	= 3,
+	["Disease"]	= 2,
+	["Poison"]	= 1,
+}
+
+local DispellFilter
+do
+	local dispellClasses = {
+		["DRUID"] = {
+			["Magic"] = false,
+			["Curse"] = true,
+			["Poison"] = true,
+		},
+		["MAGE"] = {
+			["Curse"] = true,
+		},
+		["MONK"] = {
+			["Magic"] = false,
+			["Poison"] = true,
+			["Disease"] = true,
+		},
+		["PALADIN"] = {
+			["Magic"] = false,
+			["Poison"] = true,
+			["Disease"] = true,
+		},
+		["PRIEST"] = {
+			["Magic"] = false,
+			["Disease"] = false,
+		},
+		["SHAMAN"] = {
+			["Magic"] = false,
+			["Curse"] = true,
+		},
 	}
-	
-function CreateShadow(f)
-	if f.shadow then return end
-	local shadow = CreateFrame("Frame", nil, f)
-	shadow:SetFrameLevel(1)
-	shadow:SetFrameStrata(f:GetFrameStrata())
-	shadow:SetPoint("TOPLEFT", -4, 4)
-	shadow:SetPoint("BOTTOMRIGHT", 4, -4)
-	shadow:SetBackdrop(shadows)
-	shadow:SetBackdropColor(0, 0, 0, 0)
-	shadow:SetBackdropBorderColor(0,0,0, 1)
-	f.shadow = shadow
-	return shadow
+
+	DispellFilter = dispellClasses[T.class] or {}
 end
 
-function frame1px1(f)
-	f:SetBackdrop({
-		bgFile =  [=[Interface\ChatFrame\ChatFrameBackground]=],
-        edgeFile = "Interface\\Buttons\\WHITE8x8", edgeSize = 1, 
-		insets = {left = -1, right = -1, top = -1, bottom = -1} 
-	})
-	f:SetPoint("TOPLEFT", -2, 2)
-	f:SetPoint("BOTTOMRIGHT", 2, -2)
-	f:SetBackdropColor(unpack(cfg.backdropcolor))
-	f:SetBackdropBorderColor(unpack(cfg.bordercolor))
-end
+local function CheckSpec()
+	local spec = GetSpecialization()
 
-local createAuraIcon = function(debuffs)
-	local button = CreateFrame("Button", nil, debuffs)
-	button:EnableMouse(false)
-	button:SetFrameLevel(30)
-	
-	button:SetSize(debuffs.size, debuffs.size)
-
-	local icon = button:CreateTexture(nil, "BACKGROUND")
-	
-	--icon:SetAllPoints(button)
-	icon:SetPoint("TOPLEFT",button,"TOPLEFT",-1,1)
-	icon:SetPoint("BOTTOMRIGHT",button,"BOTTOMRIGHT",1.3,-1.3)
-	icon:SetTexCoord(.15, .8, .15, .8)
-
-	local count = button:CreateFontString(nil, "OVERLAY")
-	count:SetFont(cfg.font,10,"OUTLINE")
-	count:SetShadowColor(0,0,0,0.8)
-	count:SetShadowOffset(1,-1)
-	count:SetPoint("LEFT", button, "BOTTOM", 3, 2)
-
-	local overlay = button:CreateTexture(nil, "OVERLAY")
-	overlay:SetTexture(cfg.debuffBorder)
-
-	overlay:SetTexCoord(0.03, 0.97, 0.03, 0.97)
-	button.overlay = overlay
-	
-	button:SetPoint("BOTTOMLEFT", debuffs, "BOTTOMLEFT")
-
-	button.parent = debuffs
-	button.icon = icon
-	button.count = count
-	button:Hide()
-	
-	debuffs.button = button
-	
-	local h = CreateFrame("Frame", nil, button)
-	h:SetFrameLevel(29)
-	h:SetPoint("TOPLEFT",-5,5)
-	h:SetPoint("BOTTOMRIGHT",5,-5)
-	frame1px1(h)
-	CreateShadow(h)
-end
-
-local updateDebuff = function(icon, texture, count, dtype, duration, timeLeft)
-	local color = DebuffTypeColor[dtype] or DebuffTypeColor.none
-
-	icon.overlay:SetVertexColor(color.r, color.g, color.b)
-	icon.overlay:Show()
-
-	icon.icon:SetTexture(texture)
-	icon.count:SetText((count > 1 and count))
-end
-
-local updateIcon = function(unit, debuffs)
-	local cur
-	local hide = true
-	local index = 1
-	while true do
-		local name, rank, texture, count, dtype, duration, timeLeft, caster, isStealable, shouldConsolidate, spellID = UnitAura(unit, index, 'HARMFUL')
-		if not name then break end
-		
-		local icon = debuffs.button
-		local show = debuffs.CustomFilter(debuffs, unit, icon, name, rank, texture, count, dtype, duration, timeLeft, caster, isStealable, shouldConsolidate, spellID)
-		
-		if(show) then
-			if not cur then
-				cur = icon.priority
-				updateDebuff(icon, texture, count, dtype, duration, timeLeft)
-			else
-				if icon.priority > cur then
-					updateDebuff(icon, texture, count, dtype, duration, timeLeft)
-				end
-			end
-			
-			icon:Show()
-			hide = false
+	if T.class == "DRUID" then
+		if spec == 4 then
+			DispellFilter.Magic = true
+		else
+			DispellFilter.Magic = false
 		end
-		
-		index = index + 1
+	elseif T.class == "MONK" then
+		if spec == 2 then
+			DispellFilter.Magic = true
+		else
+			DispellFilter.Magic = false
+		end
+	elseif T.class == "PALADIN" then
+		if spec == 1 then
+			DispellFilter.Magic = true
+		else
+			DispellFilter.Magic = false
+		end
+	elseif T.class == "PRIEST" then
+		if spec == 3 then
+			DispellFilter.Magic = false
+			DispellFilter.Disease = false
+		else
+			DispellFilter.Magic = true
+			DispellFilter.Disease = true
+		end
+	elseif T.class == "SHAMAN" then
+		if spec == 3 then
+			DispellFilter.Magic = true
+		else
+			DispellFilter.Magic = false
+		end
 	end
-	if hide then
-		debuffs.button:Hide()
+end
+
+local function formatTime(s)
+	if s > 60 then
+		return format("%dm", s / 60), s % 60
+	else
+		return format("%d", s), s - floor(s)
+	end
+end
+
+local abs = math.abs
+local function OnUpdate(self, elapsed)
+	self.elapsed = (self.elapsed or 0) + elapsed
+	if self.elapsed >= 0.1 then
+		local timeLeft = self.expirationTime - GetTime()
+		if self.reverse then timeLeft = abs((self.expirationTime - GetTime()) - self.duration) end
+		if timeLeft > 0 then
+			local text = formatTime(timeLeft)
+			self.time:SetText(text)
+		else
+			self:SetScript("OnUpdate", nil)
+			self.time:Hide()
+		end
+		self.elapsed = 0
+	end
+end
+
+local UpdateDebuffFrame = function(rd)
+	if rd.index and rd.type and rd.filter then
+		local name, rank, icon, count, debuffType, duration, expirationTime, _, _, _, spellId, _, isBossDebuff = UnitAura(rd.__owner.unit, rd.index, rd.filter)
+
+		if rd.icon then
+			rd.icon:SetTexture(icon)
+			rd.icon:Show()
+		end
+
+		if rd.count then
+			if count and (count > 1) then
+				rd.count:SetText(count)
+				rd.count:Show()
+			else
+				rd.count:Hide()
+			end
+		end
+
+		if spellId and T.RaidDebuffsReverse[spellId] then
+			rd.reverse = true
+		else
+			rd.reverse = nil
+		end
+
+		if rd.time then
+			rd.duration = duration
+			if duration and (duration > 0) then
+				rd.expirationTime = expirationTime
+				rd.nextUpdate = 0
+				rd:SetScript("OnUpdate", OnUpdate)
+				rd.time:Show()
+			else
+				rd:SetScript("OnUpdate", nil)
+				rd.time:Hide()
+			end
+		end
+
+		if rd.cd then
+			if duration and (duration > 0) then
+				rd.cd:SetCooldown(expirationTime - duration, duration)
+				rd.cd:Show()
+			else
+				rd.cd:Hide()
+			end
+		end
+
+		local c = DispellColor[debuffType] or DispellColor.none
+		if C.aura.debuff_color_type == true then
+			rd:SetBackdropBorderColor(c[1], c[2], c[3])
+		end
+
+		if not rd:IsShown() then
+			rd:Show()
+		end
+	else
+		if rd:IsShown() then
+			rd:Hide()
+		end
 	end
 end
 
 local Update = function(self, event, unit)
-	if(self.unit ~= unit) then return end
+	if unit ~= self.unit then return end
+	local rd = self.RaidDebuffs
+	rd.priority = invalidPrio
 
-	local debuffs = self.raidDebuffs
-	if(debuffs) then
-		updateIcon(unit, debuffs)	
+	for filter in next, (rd.Filters or auraFilters) do
+		local i = 0
+		while(true) do
+			i = i + 1
+			local name, rank, icon, count, debuffType, duration, expirationTime, _, _, _, spellId, _, isBossDebuff = UnitAura(unit, i, filter)
+			if not name then break end
+
+			if rd.ShowBossDebuff and isBossDebuff then
+				local prio = rd.BossDebuffPriority or bossDebuffPrio
+				if prio and prio > rd.priority then
+					rd.priority = prio
+					rd.index = i
+					rd.type = "Boss"
+					rd.filter = filter
+				end
+			end
+
+			if rd.ShowDispellableDebuff and debuffType then
+				local disPrio = rd.DispellPriority or DispellPriority
+				local disFilter = rd.DispellFilter or DispellFilter
+				local prio
+
+				if rd.FilterDispellableDebuff and disFilter then
+					prio = disFilter[debuffType] and disPrio[debuffType]
+				else
+					prio = disPrio[debuffType]
+				end
+
+				if prio and (prio > rd.priority) then
+					rd.priority = prio
+					rd.index = i
+					rd.type = "Dispel"
+					rd.filter = filter
+				end
+			end
+
+			local prio = rd.Debuffs and rd.Debuffs[rd.MatchBySpellName and name or spellId]
+			local prioPvP
+			if C.raidframe.plugins_pvp_debuffs == true then
+				prioPvP = rd.Debuffs and T.PvPDebuffs[rd.MatchBySpellName and name or spellId]
+			end
+			if not T.RaidDebuffsIgnore[spellId] and (prio and (prio > rd.priority) or prioPvP and (prioPvP > rd.priority)) then
+				if (prio and not prioPvP) or ((prio and prioPvP) and prio > prioPvP) then
+					rd.priority = prio
+				else
+					rd.priority = prioPvP
+				end
+				rd.index = i
+				rd.type = "Custom"
+				rd.filter = filter
+			end
+		end
 	end
+
+	if rd.priority == invalidPrio then
+		rd.index = nil
+		rd.filter = nil
+		rd.type = nil
+	end
+
+	return UpdateDebuffFrame(rd)
+end
+
+local Path = function(self, ...)
+	return (self.RaidDebuffs.Override or Update) (self, ...)
+end
+
+local ForceUpdate = function(element)
+	return Path(element.__owner, "ForceUpdate", element.__owner.unit)
 end
 
 local Enable = function(self)
-	if(self.raidDebuffs) then
-		createAuraIcon(self.raidDebuffs)
-		self:RegisterEvent("UNIT_AURA", Update)
-
+	local rd = self.RaidDebuffs
+	if rd then
+		self:RegisterEvent("UNIT_AURA", Path)
+		rd.ForceUpdate = ForceUpdate
+		rd.__owner = self
 		return true
 	end
+	self:RegisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
+	CheckSpec()
 end
 
 local Disable = function(self)
-	if(self.raidDebuffs) then
-		self:UnregisterEvent("UNIT_AURA", Update)
+	if self.RaidDebuffs then
+		self:UnregisterEvent("UNIT_AURA", Path)
+		self.RaidDebuffs:Hide()
+		self.RaidDebuffs.__owner = nil
 	end
+	self:UnregisterEvent("PLAYER_TALENT_UPDATE", CheckSpec)
 end
 
-oUF:AddElement('raidDebuffs', Update, Enable, Disable)
+oUF:AddElement("RaidDebuffs", Update, Enable, Disable)
